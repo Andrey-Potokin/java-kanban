32f8eb26fe5b14d89b12cc59529900370a6c5a9e
Andrey Potokin <potokin.and@yandex.ru>
2024-11-28 15:10:57 +0300

    Сделать class DoublyLinkedList вложенным

--- src/InMemoryHistoryManager.java
+++ src/InMemoryHistoryManager.java
@@ -25,67 +25,67 @@ public class InMemoryHistoryManager implements HistoryManager {
         tasksHistory.remove(node);
         nodes.remove(node.data.getID());
     }
-}
 
-class DoublyLinkedList<T> {
+    private static class DoublyLinkedList<T> {
 
-    private Node<T> head;
-    private Node<T> tail;
-    private int size = 0;
+        private Node<T> head;
+        private Node<T> tail;
+        private int size = 0;
 
-    // добавляет задачу в конец списка
-    public void linkLast(T element) {
-        final Node<T> oldTail = tail;
-        final Node<T> newNode = new Node<>(oldTail, element, null);
-        tail = newNode;
-        if (oldTail == null) {
-            head = newNode;
-        } else {
-            oldTail.next = newNode;
-            newNode.prev = oldTail;
-        }
-        size++;
-    }
-
-    // собирает все задачи
-    public List<Task> getTask() {
-        List<Task> tasks = new ArrayList<>();
-        Node<T> current = head;
-        while (current != null) {
-            tasks.add((Task) current.data);
-            current = current.next;
-        }
-        return tasks;
-    }
-
-    // возвращает последний узел
-    public Node<T> getLastNode() {
-        return tail;
-    }
-
-    public void remove(Node<T> node) {
-        if (node == null) return;
-
-        Node<T> prevNode = node.prev;
-        Node<T> nextNode = node.next;
-
-        if (prevNode == null) {
-            head = nextNode;
-        } else {
-            prevNode.next = nextNode;
-            node.prev = null;
+        // добавляет задачу в конец списка
+        public void linkLast(T element) {
+            final Node<T> oldTail = tail;
+            final Node<T> newNode = new Node<>(oldTail, element, null);
+            tail = newNode;
+            if (oldTail == null) {
+                head = newNode;
+            } else {
+                oldTail.next = newNode;
+                newNode.prev = oldTail;
+            }
+            size++;
         }
 
-        if (nextNode == null) {
-            tail = prevNode;
-        } else {
-            nextNode.prev = prevNode;
-            node.next = null;
+        // собирает все задачи
+        public List<Task> getTask() {
+            List<Task> tasks = new ArrayList<>();
+            Node<T> current = head;
+            while (current != null) {
+                tasks.add((Task) current.data);
+                current = current.next;
+            }
+            return tasks;
         }
-        size--;
-    }
 
-    public int size() {
-        return this.size;
+        // возвращает последний узел
+        public Node<T> getLastNode() {
+            return tail;
+        }
+
+        public void remove(Node<T> node) {
+            if (node == null) return;
+
+            Node<T> prevNode = node.prev;
+            Node<T> nextNode = node.next;
+
+            if (prevNode == null) {
+                head = nextNode;
+            } else {
+                prevNode.next = nextNode;
+                node.prev = null;
+            }
+
+            if (nextNode == null) {
+                tail = prevNode;
+            } else {
+                nextNode.prev = prevNode;
+                node.next = null;
+            }
+            size--;
+        }
+
+        public int size() {
+            return this.size;
+        }
     }
 }
